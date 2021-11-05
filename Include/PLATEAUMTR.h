4a8981b9c8602354db89e0d6c8d6d6c59c874c95
Abderazzak Moumen <93351660+AbderazzakMoumen@users.noreply.github.com>
2021-11-05 10:12:52 +0100

    Ajout files PLATEAUMTR

---
+++ Include/PLATEAUMTR.h
@@ -0,0 +1,13 @@
+#ifndef SERVOMTR_H
+#define SERVOMTR_H
+
+#include "stm32f10x.h"
+#include "TIMER.h"
+#include "GPIO.h"
+
+void Initialisation_SERVOMTR();
+void Configuration_SERVOMTR();
+
+
+
+#endif
